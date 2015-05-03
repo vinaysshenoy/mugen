169f07797e5200d6aee982b748b14c437af1a82f
Imran <imohammed23@gmail.com>
2015-05-03 08:49:26 +0530

    add docs

--- library/src/main/java/com/mugen/attachers/RecyclerViewPositionHelper.java
+++ library/src/main/java/com/mugen/attachers/RecyclerViewPositionHelper.java
@@ -26,25 +26,58 @@ class RecyclerViewPositionHelper {
         return new RecyclerViewPositionHelper(recyclerView);
     }
 
+    /**
+     * Returns the adapter item count.
+     *
+     * @return The total number on items in a layout manager
+     */
     public int getItemCount() {
         return layoutManager == null ? 0 : layoutManager.getItemCount();
     }
 
+    /**
+     * Returns the adapter position of the first visible view. This position does not include
+     * adapter changes that were dispatched after the last layout pass.
+     *
+     * @return The adapter position of the first visible item or {@link RecyclerView#NO_POSITION} if
+     * there aren't any visible items.
+     */
     public int findFirstVisibleItemPosition() {
         final View child = findOneVisibleChild(0, layoutManager.getChildCount(), false, true);
         return child == null ? NO_POSITION : recyclerView.getChildAdapterPosition(child);
     }
 
+    /**
+     * Returns the adapter position of the first fully visible view. This position does not include
+     * adapter changes that were dispatched after the last layout pass.
+     *
+     * @return The adapter position of the first fully visible item or
+     * {@link RecyclerView#NO_POSITION} if there aren't any visible items.
+     */
     public int findFirstCompletelyVisibleItemPosition() {
         final View child = findOneVisibleChild(0, layoutManager.getChildCount(), true, false);
         return child == null ? NO_POSITION : recyclerView.getChildAdapterPosition(child);
     }
 
+    /**
+     * Returns the adapter position of the last visible view. This position does not include
+     * adapter changes that were dispatched after the last layout pass.
+     *
+     * @return The adapter position of the last visible view or {@link RecyclerView#NO_POSITION} if
+     * there aren't any visible items
+     */
     public int findLastVisibleItemPosition() {
         final View child = findOneVisibleChild(layoutManager.getChildCount() - 1, -1, false, true);
         return child == null ? NO_POSITION : recyclerView.getChildAdapterPosition(child);
     }
 
+    /**
+     * Returns the adapter position of the last fully visible view. This position does not include
+     * adapter changes that were dispatched after the last layout pass.
+     *
+     * @return The adapter position of the last fully visible view or
+     * {@link RecyclerView#NO_POSITION} if there aren't any visible items.
+     */
     public int findLastCompletelyVisibleItemPosition() {
         final View child = findOneVisibleChild(layoutManager.getChildCount() - 1, -1, true, false);
         return child == null ? NO_POSITION : recyclerView.getChildAdapterPosition(child);
